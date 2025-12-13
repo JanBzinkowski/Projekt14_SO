501a703b7f5578d33dead15b474e3b4c3e790782
Jan Bzinkowski <janek.bzin112@gmail.com>
2025-12-13 14:45:03 +0100

    Stworzenie generatora klientow

--- src/generator_klientow.cpp
+++ src/generator_klientow.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <ctime>
+#include <csignal>
 
-int main() {}
+#include "../include/Shared_memory.h"
+
+int main() {
+	srand(time(nullptr));
+	int shared = shm_open("/shmem", O_RDWR, 0666);
+	if (shared == -1) {
+		perror("shared memory open");
+		exit(1);
+	}
+
+	auto *shared_mem_flags = static_cast<SharedMem *>(mmap(nullptr, sizeof(SharedMem), PROT_READ, MAP_SHARED, shared, 0));
+
+	signal(SIGCHLD, SIG_IGN);
+	int pid;
+
+	while (shared_mem_flags->end_program == false) {
+		pid = fork();
+		if (pid == -1) {
+			perror("fork, generator klientow");
+		}
+		else if (pid == 0) {
+			execl("./klient", "klient", NULL);
+			perror("execl failed");
+			exit(1);
+		}
+		else {
+			sleep(rand() % 100);
+		}
+	}
+}
